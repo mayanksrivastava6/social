1bae4740494a6cc3885b7c49f8f0f1b73986fc4c
mayanksrivastava6 <mayanksrivastava61103@gmail.com>
2023-04-22 02:31:00 +0530

    ejs home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for media</h1>')
+    // return res.end('<h1>Express is up for media</h1>')
+    return res.render('home',{
+        ttitle: "home"
+    });
+
 }
