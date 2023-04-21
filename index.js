7f64d37d05d4663c6c3cb11e916063fc411d619f
mayanksrivastava6 <mayanksrivastava61103@gmail.com>
2023-04-21 17:12:24 +0530

    'add'

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 4000;
    
+//use express router
+app.use('/' , require('./routes'));
 
 app.listen(port, function(err){
     if(err){
         console.log('Error in running the server',{err});
 
     }
-    
+
     console.log('Server is running on port:',{port});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const homeController = require('../controllers/home_controller')
 
+console.log('router loaded');
+
+
+
+router.get('/', homeController.home )
 
 
 
