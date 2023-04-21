5bf8d0eb3447d60be9d16dff7f2d306d069689d9
mayanksrivastava6 <mayanksrivastava61103@gmail.com>
2023-04-22 02:21:39 +0530

    ejs start

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 4000;
 //use express router
 app.use('/' , require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('view', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log('Error in running the server',{err});
