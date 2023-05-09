a89fd2708e43a30695859d2befa2642ce72ab50a
mayanksrivastava6 <mayanksrivastava61103@gmail.com>
2023-05-09 19:20:19 +0530

    may 9th second

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,17 +1,28 @@
 const Post = require('../models/post');
-// const commentModel=require('../models/comment');
-
+const Comment = require('../models/comment');
 module.exports.create = function(req, res){
       Post.create({
         content: req.body.content,
         user: req.user._id,
-        // name: req.user.name,
-        // comments: req.user.comments
-      },function(err,post){
-        // comment.save();
-        if(err){console.log('error in creating a post'); return;}
+       },function(err,post){
+      if(err){console.log('error in creating a post'); return;}
 
         return res.redirect('back');
+      });
+}
 
-      })
+
+module.exports.destroy = function(req, res){
+  Post.findById(req.params.id, function(err, post){
+// .id means converting the objecct id into string
+    if(post.user == req.user.id){
+      post.remove();
+        
+        Comment.deleteMany({post: req.params.id}, function(err){
+          return res.redirect('back');
+        });
+    }else{
+      return res.redirect('back');
+    }
+  });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const passport =require('passport');
 
-const postController =require('../controllers/post_controller')
+const postsController =require('../controllers/post_controller')
 
-router.post('/create',passport.checkAuthentication, postController.create);
-
+router.post('/create',passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 
 module.exports = router;
